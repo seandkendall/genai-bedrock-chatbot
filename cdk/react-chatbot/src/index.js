187ad57c5794685f5b499d33f245ab02830dd031
Ritik Khatwani <ritikkh@amazon.com>
2024-11-26 17:12:54 -0500

    Updated to use signInWithRedirect for external login provider

--- cdk/react-chatbot/src/index.js
+++ cdk/react-chatbot/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Authenticator } from '@aws-amplify/ui-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Authenticator.Provider>
+      <App />
+    </Authenticator.Provider>
   </React.StrictMode>
 );
 
