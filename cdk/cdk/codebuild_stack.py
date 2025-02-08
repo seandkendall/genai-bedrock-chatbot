fefb303cae11f702795145299f10f20d9f8e33be
Sean Kendall <seandall@amazon.com>
2025-02-08 16:39:50 -0700

    Working model import

--- cdk/cdk/codebuild_stack.py
+++ cdk/cdk/codebuild_stack.py
@@ -22,7 +22,48 @@ class CodeBuildStack(Stack):
                 deploy_deep_seek = True
                 
         if deploy_deep_seek:
-            # IAM Role for CodeBuild
+            # Create the trust policy statement
+            bedrock_import_model_role_trust_policy_statement = iam.PolicyStatement(
+                effect=iam.Effect.ALLOW,
+                actions=["sts:AssumeRole"],
+                principals=[iam.ServicePrincipal("bedrock.amazonaws.com")],
+                conditions={
+                    "StringEquals": {"aws:SourceAccount": self.account},
+                    "ArnEquals": {
+                        "aws:SourceArn": f"arn:aws:bedrock:{self.region}:{self.account}:model-import-job/*"
+                    },
+                },
+            )
+
+            # Create the inline policy
+            bedrock_import_model_role_inline_policy = iam.PolicyDocument()
+            bedrock_import_model_role_inline_policy.add_statements(
+                iam.PolicyStatement(
+                    effect=iam.Effect.ALLOW,
+                    actions=["s3:GetObject", "s3:ListBucket"],
+                    resources=[
+                        "arn:aws:s3:::{custom_model_s3_bucket_name}",
+                        "arn:aws:s3:::{custom_model_s3_bucket_name}/*",
+                    ],
+                    conditions={
+                        "StringEquals": {"aws:ResourceAccount": self.account}
+                    },
+                )
+            )
+
+            # Create the IAM Role
+            bedrock_import_model_role = iam.Role(
+                self,
+                "BedrockImportModelRole",
+                assumed_by=iam.ServicePrincipal("bedrock.amazonaws.com"),
+                inline_policies={"InlinePolicy": bedrock_import_model_role_inline_policy},
+                max_session_duration=Duration.hours(1),
+            )
+
+            # Attach the trust policy statement to the assume role policy
+            if bedrock_import_model_role.assume_role_policy:
+                bedrock_import_model_role.assume_role_policy.add_statements(bedrock_import_model_role_trust_policy_statement)
+
             codebuild_role = iam.Role(
                 self, "GenAIChatBotCustomModelImportCodeBuildRole",
                 assumed_by=iam.ServicePrincipal("codebuild.amazonaws.com")
@@ -39,52 +80,25 @@ class CodeBuildStack(Stack):
             # Grant permissions to access the S3 bucket
             s3.Bucket.from_bucket_name(self, "GenAIChatBotCustomModelImportTargetBucket", custom_model_s3_bucket_name).grant_read_write(codebuild_role)
             
-            # Define the build spec as a dictionary
-            build_spec_dict = {
-                "version": "0.2",
-                "phases": {
-                    "build": {
-                        "commands": [
-                            # Group commands in a YAML block scalar
-                            f"""|
-                            # Define variables
-                            S3_BUCKET="{custom_model_s3_bucket_name}"
-                            S3_KEY="DeepSeek-R1-Distill-Llama-8B"
-                            
-                            # Check if the folder exists in the S3 bucket
-                            if aws s3 ls "s3://$S3_BUCKET/$S3_KEY" > /dev/null 2>&1; then
-                            echo "Folder $S3_KEY already exists in bucket $S3_BUCKET. Exiting successfully."
-                            aws codebuild stop-build --id $CODEBUILD_BUILD_ID
-                            exit 0
-                            fi
 
-                            # Proceed with the rest of the commands if the folder does not exist
-                            sudo dnf update -y
-                            sudo dnf install git-lfs -y
-                            git lfs install
-                            git clone --filter=blob:none --no-checkout https://huggingface.co/deepseek-ai/DeepSeek-R1-Distill-Llama-8B
-                            cd DeepSeek-R1-Distill-Llama-8B
-                            git checkout main
-                            git lfs pull
-                            cd ..
-                            aws s3 sync --exclude '.git*' DeepSeek-R1-Distill-Llama-8B "s3://$S3_BUCKET/$S3_KEY/"
-                            """
-                        ]
-                    }
-                }
-            }
-    
             # Create CodeBuild Project
+            deepseek_model_8B_url= "https://huggingface.co/deepseek-ai/DeepSeek-R1-Distill-Llama-8B"
+            deepseek_model_70B_url= "https://huggingface.co/deepseek-ai/DeepSeek-R1-Distill-Llama-70B"
+            model_urls = [deepseek_model_8B_url, deepseek_model_70B_url]
             codebuild_project = codebuild.Project(
                 self, "GenAIChatBotCustomModelImportCodeBuildProject",
                 role=codebuild_role,
                 build_spec=codebuild.BuildSpec.from_asset("cdk/buildspec.yml"),
                 environment=codebuild.BuildEnvironment(
-                    build_image=codebuild.LinuxBuildImage.STANDARD_7_0,
+                    build_image=codebuild.LinuxBuildImage.from_code_build_image_id('aws/codebuild/amazonlinux-x86_64-standard:5.0'),
                     compute_type=codebuild.ComputeType.LARGE,
                     privileged=True,
                     environment_variables={
-                        "S3_BUCKET": codebuild.BuildEnvironmentVariable(value=custom_model_s3_bucket_name)
+                        "S3_BUCKET": codebuild.BuildEnvironmentVariable(value=custom_model_s3_bucket_name),
+                        "MODEL_URL": codebuild.BuildEnvironmentVariable(value=deepseek_model_8B_url),
+                        # "MODEL_URL": codebuild.BuildEnvironmentVariable(value=",".join(model_urls)),
+                        "ROLE_ARN": codebuild.BuildEnvironmentVariable(value=codebuild_role.role_arn),
+                        "MODEL_IMPORT_ROLE_ARN": codebuild.BuildEnvironmentVariable(value=bedrock_import_model_role.role_arn)
                     }
                 )
             )
@@ -115,5 +129,24 @@ class CodeBuildStack(Stack):
                         "codebuild:StartBuild","codebuild:StopBuild"
                     ],
                     resources=[codebuild_project.project_arn]
+                )
+            )
+            codebuild_role.add_to_policy(
+                iam.PolicyStatement(
+                    effect=iam.Effect.ALLOW,
+                    actions=[
+                        "bedrock:CreateModelImportJob","iam:PassRole"
+                    ],
+                    resources=["*"]
+                )
+            )
+            bucket_arn = s3.Bucket.from_bucket_name(self, "custom_model_s3_bucket", custom_model_s3_bucket_name).bucket_arn
+            codebuild_role.add_to_policy(
+                iam.PolicyStatement(
+                    effect=iam.Effect.ALLOW,
+                    actions=[
+                        "s3:ListBucket"
+                    ],
+                    resources=[bucket_arn]
                 )
             )
