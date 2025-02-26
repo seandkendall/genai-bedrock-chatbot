3cfca6ad9f7640da964ff683ef2f8b8efdc144b9
Sean Kendall <seandall@amazon.com>
2025-02-26 14:07:51 -0700

    Fixing Null variable bug

--- cdk/app.py
+++ cdk/app.py
@@ -18,11 +18,14 @@ chatbot_stack = ChatbotWebsiteStack(app, "ChatbotWebsiteStack")
 aws_application = 'NoApplicationCreatedyet'
 if chatbot_stack.aws_application is not None and len(chatbot_stack.aws_application) > 1:
     aws_application = chatbot_stack.aws_application
+imported_models = ""
+if chatbot_stack.imported_models is not None and len(chatbot_stack.imported_models) > 1:
+    imported_models = chatbot_stack.imported_models    
 
 custom_model_s3_bucket_name = chatbot_stack.custom_model_import_bucket.bucket_name
 codebuild_stack = CodeBuildStack(app, "ChatbotWebsiteStack-CodeBuildStack",
                                  custom_model_s3_bucket_name=custom_model_s3_bucket_name,
-                                 imported_models=chatbot_stack.imported_models,
+                                 imported_models=imported_models,
                                  project=f"genai-bedrock-chatbot-{region}",
                                  aws_application=aws_application)
 if deploy_service_catalog_application:
